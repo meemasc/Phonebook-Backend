08526790732a80c5128c121aed439dbf753e75b6
meemasc <45151515+meemasc@users.noreply.github.com>
2021-09-21 01:46:54 +0300

    uibuild

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const morgan = require('morgan')
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 morgan.token('dataToken', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :dataToken'))
 
