58209d56e117e050dda88e70070146b83f61a80f
meemasc <45151515+meemasc@users.noreply.github.com>
2021-09-27 10:04:45 +0300

    uibuild

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
   if (body.name === undefined) {
-    return response.status(400).json({ error: 'content missing' })
+    return response.status(400).json({ error: 'name missing' })
   }
 
   const person = new Person({
@@ -74,7 +74,7 @@ app.put('/api/persons/:id', (request, response, next) => {
     number: body.number,
   }
 
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+  Person.findByIdAndUpdate(request.params.id, person, { new: true, runValidators: true })
     .then(updatedNote => {
       response.json(updatedNote)
     })
--- models/person.js
+++ models/person.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 require('dotenv').config()
+const uniqueValidator = require('mongoose-unique-validator');
 
 const url = process.env.MONGODB_URI
 
@@ -14,8 +15,16 @@ mongoose.connect(url)
 })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String
+  name: {
+    type: String,
+    unique: true,
+    required: true,
+    minLength: 3
+  },
+  number: {
+    type: String,
+    minlength: 8
+  }
 })
 
 personSchema.set('toJSON', {
